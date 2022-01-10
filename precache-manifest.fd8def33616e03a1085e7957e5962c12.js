44685514ab24d0195638d090db4c458250da6f21
Sav <sav.dontamsetti@gmail.com>
2022-01-09 20:51:45 -0500

    Updates

---
+++ precache-manifest.fd8def33616e03a1085e7957e5962c12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367f51f8361635c75365963cdb678ccf",
+    "url": "/cyph-dapp/index.html"
+  },
+  {
+    "revision": "0fda41fd4116fe0aef66",
+    "url": "/cyph-dapp/static/css/2.87a71177.chunk.css"
+  },
+  {
+    "revision": "6a64be621bbcacb512a1",
+    "url": "/cyph-dapp/static/css/main.1619c104.chunk.css"
+  },
+  {
+    "revision": "0fda41fd4116fe0aef66",
+    "url": "/cyph-dapp/static/js/2.843b9eed.chunk.js"
+  },
+  {
+    "revision": "226eaecc22d857bed5f4762de5e9ca26",
+    "url": "/cyph-dapp/static/js/2.843b9eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a64be621bbcacb512a1",
+    "url": "/cyph-dapp/static/js/main.e34008b3.chunk.js"
+  },
+  {
+    "revision": "e63109c34fdeb2c22de9",
+    "url": "/cyph-dapp/static/js/runtime-main.7c396d08.js"
+  }
+]);
